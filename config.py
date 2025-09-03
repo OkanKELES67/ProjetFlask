ac3db5845db938e468eab846deada75375aba077
Okan <kelesokan57@gmail.com>
2025-09-03 15:25:52 +0200

    finalisation du tp1(j'ai oublier le readme de la premiere sceance)

---
+++ config.py
@@ -0,0 +1,5 @@
+#>>>import random, string, os
+#>>>"".join([random.choice(string.printable) for _ in os.urandom(24) ] )
+SECRET_KEY = "2lzUl{$*D6#`8uXqlU."
+ABOUT = "Bienvenue sur la page à propos de Flask !"
+CONTACT = "Contactez-moi à l'adresse email suivante :"
---
+++ monApp/views.py
@@ -0,0 +1,13 @@
+from .app import app
+@app.route('/')
+def index():
+    return "Hello world !"
+
+@app.route('/about')
+def about():
+    return app.config['ABOUT']
+@app.route('/contact')
+def contact():
+    return app.config['CONTACT']
+if __name__ == "__main__":
+    app.run()
